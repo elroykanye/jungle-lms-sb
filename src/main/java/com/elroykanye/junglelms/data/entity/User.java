e95e074f0f75131cad076196ecc626fc8730fdeb
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:41:00 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/data/entity/User.java

---
+++ src/main/java/com/elroykanye/junglelms/data/entity/User.java
@@ -0,0 +1,11 @@
+package com.elroykanye.junglelms.data.entity;
+
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+	 private Long userId;
+}
