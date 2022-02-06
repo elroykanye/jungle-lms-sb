03d68e3e40423786a4c04543a5beb4dc4c1f13cb
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:41:00 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/data/repository/CourseMaterialAttachmentRepository.java

---
+++ src/main/java/com/elroykanye/junglelms/data/repository/CourseMaterialAttachmentRepository.java
@@ -0,0 +1,6 @@
+package com.elroykanye.junglelms.data.repository;
+
+
+@Repository
+public interface CourseMaterialAttachmentRepository extends JpaRepository<CourseMaterialAttachmentRepository, Long> {
+}
