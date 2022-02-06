8d30eb03658aa85a276f60a141b75d089635aafb
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:41:00 +0100

    initialise repo - ek: added ./src/test/java/com/elroykanye/junglelms/JungleLmsApplicationTests.java

---
+++ src/test/java/com/elroykanye/junglelms/JungleLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elroykanye.junglelms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungleLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
