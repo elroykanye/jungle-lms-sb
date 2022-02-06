8888f897d5a57808b9b512233823ca6960fa5c2b
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:41:00 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/JungleLmsApplication.java

---
+++ src/main/java/com/elroykanye/junglelms/JungleLmsApplication.java
@@ -0,0 +1,13 @@
+package com.elroykanye.junglelms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungleLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JungleLmsApplication.class, args);
+    }
+
+}
