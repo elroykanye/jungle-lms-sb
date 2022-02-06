d7cecfffddb92c86924dbdeeb95804d527d13af5
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:41:00 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/data/entity/Enrolment.java

---
+++ src/main/java/com/elroykanye/junglelms/data/entity/Enrolment.java
@@ -0,0 +1,11 @@
+package com.elroykanye.junglelms.data.entity;
+
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "enrolment")
+public class Enrolment {
+	 private Long enrolmentId;
+}
