7a7acc2911de4b49305efe8c1702f7c8a3a547ec
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:41:00 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/data/entity/CourseMaterialAttachment.java

---
+++ src/main/java/com/elroykanye/junglelms/data/entity/CourseMaterialAttachment.java
@@ -0,0 +1,11 @@
+package com.elroykanye.junglelms.data.entity;
+
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "coursematerialattachment")
+public class CourseMaterialAttachment {
+	 private Long coursematerialattachmentId;
+}
