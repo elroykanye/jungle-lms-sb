f493455801fdbe9c634721516b16a493f764ec35
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:40:59 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/api/controller/CourseController.java

---
+++ src/main/java/com/elroykanye/junglelms/api/controller/CourseController.java
@@ -0,0 +1,12 @@
+package com.elroykanye.junglelms.api.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/api/course")
+public class CourseController {
+}
