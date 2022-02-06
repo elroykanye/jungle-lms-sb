db19326decc4bca5243de506ae1ab00939984edf
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:40:59 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/business/service/impl/TeacherServiceImpl.kt

---
+++ src/main/java/com/elroykanye/junglelms/business/service/impl/TeacherServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.elroykanye.junglelms.business.service.impl
+
+import com.elroykanye.junglelms.business.service.TeacherService
+import org.springframework.stereotype.Service
+
+@Service
+class TeacherServiceImpl: TeacherService {
+}
