3d9043cfa49c71058696173d64a26d1887122e73
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:41:00 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/data/entity/Course.java

---
+++ src/main/java/com/elroykanye/junglelms/data/entity/Course.java
@@ -0,0 +1,11 @@
+package com.elroykanye.junglelms.data.entity;
+
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "course")
+public class Course {
+	 private Long courseId;
+}
