5c6702065c935981edcc476dd7807e6ec52bb7e5
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:40:59 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/business/service/impl/FieldServiceImpl.kt

---
+++ src/main/java/com/elroykanye/junglelms/business/service/impl/FieldServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.elroykanye.junglelms.business.service.impl
+
+import com.elroykanye.junglelms.business.service.FieldService
+import org.springframework.stereotype.Service
+
+@Service
+class FieldServiceImpl: FieldService {
+}
