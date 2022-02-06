bb625fdb5cb2bd2000f978750c22c0e024180f6f
Elroy Kanye <elroykanye@gmail.com>
2022-02-06 07:40:59 +0100

    initialise repo - ek: added ./src/main/java/com/elroykanye/junglelms/business/service/impl/UserServiceImpl.kt

---
+++ src/main/java/com/elroykanye/junglelms/business/service/impl/UserServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.elroykanye.junglelms.business.service.impl
+
+import com.elroykanye.junglelms.business.service.UserService
+import org.springframework.stereotype.Service
+
+@Service
+class UserServiceImpl: UserService {
+}
