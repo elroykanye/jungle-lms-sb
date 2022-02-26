dcacfd224eb04b92aa530f8ca92f63517825face
Elroy Kanye <elroykanye@gmail.com>
2022-02-26 16:13:40 +0100

    added userservice interface methods and impls

--- src/main/java/com/elroykanye/junglelms/business/service/UserService.kt
+++ src/main/java/com/elroykanye/junglelms/business/service/UserService.kt
@@ -1,3 +1,15 @@
 package com.elroykanye.junglelms.business.service
 
-interface UserService
+import com.elroykanye.junglelms.data.entity.User
+
+interface UserService {
+    fun findAll(): List<User>
+    fun findById(id: Long): User
+    fun findByUsername(username: String): User
+
+
+    fun save(user: User): User
+
+
+    fun delete(id: Long)
+}
--- src/main/java/com/elroykanye/junglelms/business/service/impl/UserServiceImpl.kt
+++ src/main/java/com/elroykanye/junglelms/business/service/impl/UserServiceImpl.kt
@@ -1,7 +1,47 @@
 package com.elroykanye.junglelms.business.service.impl
 
 import com.elroykanye.junglelms.business.service.UserService
+import com.elroykanye.junglelms.data.entity.User
+import com.elroykanye.junglelms.data.repository.UserRepository
+import com.elroykanye.junglelms.exception.UserException
+import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class UserServiceImpl: UserService
+@RequiredArgsConstructor
+class UserServiceImpl(
+    private val userRepository: UserRepository
+): UserService {
+    override fun findAll(): List<User> {
+        return userRepository.findAll()
+    }
+
+    override fun findById(id: Long): User {
+        val userOpt: Optional<User> = userRepository.findById(id)
+        if(userOpt.isPresent) {
+            return userOpt.get()
+        } else {
+            throw UserException.UserNotFoundWithIdException(id)
+        }
+    }
+
+    override fun findByUsername(username: String): User {
+        val userOpt: Optional<User> = userRepository.findByUsername(username)
+        if(userOpt.isPresent) return userOpt.get()
+        else throw UserException.UserNotFoundWithUsernameException(username)
+    }
+
+    override fun save(user: User): User {
+        if(userRepository.findByUsername(user.username).isPresent) {
+            throw UserException.UserAlreadyExistsException(user.username)
+        }
+        return userRepository.save(user)
+    }
+
+    override fun delete(id: Long) {
+        if(userRepository.findById(id).isPresent) userRepository.deleteById(id)
+        else throw UserException.UserNotFoundWithIdException(id)
+    }
+
+}
